8083065b4912701e280598b09bd88f9a92450359
Max Wendel <maxwendelffaraujo@gmail.com>
2019-04-25 13:57:27 -0300

    Change to Kotlin

--- src/main/kotlin/com/jdev/microservice/contact/configuration/SwaggerConfig.java
+++ src/main/kotlin/com/jdev/microservice/contact/configuration/SwaggerConfig.java
@@ -1,39 +1,38 @@
-package com.jdev.microservice.contact.configuration
+package com.jdev.microservice.contact.configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import springfox.documentation.builders.PathSelectors
-import springfox.documentation.builders.RequestHandlerSelectors
-import springfox.documentation.service.ApiInfo
-import springfox.documentation.service.Contact
-import springfox.documentation.spi.DocumentationType
-import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2
-
-import java.util.Collections
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
-class SwaggerConfig {
+public class SwaggerConfig {
 
     @Bean
-    fun api(): Docket {
-        return Docket(DocumentationType.SWAGGER_2)
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo())
+                .apiInfo(apiInfo());
     }
 
-    private fun apiInfo(): ApiInfo {
-        return ApiInfo(
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
                 "Contact-API",
                 "API de cadastro de contatos.",
                 "0.0.1-SNAPSHOT",
                 "Terms of service",
-                Contact("", "", ""),
-                "License of API", "API license URL", emptyList<VendorExtension>())
+                new Contact("", "", ""),
+                "License of API", "API license URL", Collections.emptyList());
     }
 
 }
