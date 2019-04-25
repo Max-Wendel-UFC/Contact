56dddb394efdf9ee92e8c24f3bfdf9e7a8fb79cd
Max Wendel <maxwendelffaraujo@gmail.com>
2019-04-25 13:56:08 -0300

    Add ModelMapper configurations

---
+++ src/main/kotlin/com/jdev/microservice/contact/configuration/ModelMapperConfig.kt
@@ -0,0 +1,14 @@
+package com.jdev.microservice.contact.configuration
+
+import org.modelmapper.ModelMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ModelMapperConfig {
+
+    @Bean
+    fun modelMapper():ModelMapper{
+        return ModelMapper()
+    }
+}
--- src/main/kotlin/com/jdev/microservice/contact/configuration/SwaggerConfig.java
+++ src/main/kotlin/com/jdev/microservice/contact/configuration/SwaggerConfig.java
@@ -1,38 +1,39 @@
-package com.jdev.microservice.contact.configuration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+package com.jdev.microservice.contact.configuration
 
-import java.util.Collections;
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+import java.util.Collections
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfo(
+    private fun apiInfo(): ApiInfo {
+        return ApiInfo(
                 "Contact-API",
                 "API de cadastro de contatos.",
                 "0.0.1-SNAPSHOT",
                 "Terms of service",
-                new Contact("", "", ""),
-                "License of API", "API license URL", Collections.emptyList());
+                Contact("", "", ""),
+                "License of API", "API license URL", emptyList<VendorExtension>())
     }
 
 }
